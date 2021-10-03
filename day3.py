da550ce728115e7ec070b4e4b39433fac54729e0
Micah Magruder <mmagruder@gmail.com>
2021-10-03 16:49:05 -0400

    day3

---
+++ day3.py
@@ -0,0 +1,10 @@
+# Annoying Alarm 
+# cerner_2tothe5th_2021
+# Set up script as a cron job or windows task, set speakers to 100 and you'll hopefully never forget like I did. 
+# 
+
+import os
+
+while True:
+    os.system("say it is time to submit some code")
+    
